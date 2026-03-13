f3f2cf7c2d156d4cc4c8db186df3018f7f245488
Rachel Luginbill <rachelluginbill27@gmail.com>
2026-03-12 17:52:47 -0700

    Created file structure with headers and cpp's, also linked most files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*@Author: Rachel Luginbill
+@Date: March 12, 2026
+@Description: This MS users will be able to upload files and request to view them. 
+Also, you will be able to use CRUD op on all avalible files.*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "content/file_contents.h"
+#include "create/create_files.h"
+#include "delete/delete_file.h"
+#include "update/file_updator.h"
+#include "display/display_files.h"
+
+int main(){
+
+
+
+}
